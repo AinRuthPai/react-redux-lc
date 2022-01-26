c31ea209c3ba70237df21c8bddf7b41e6e8fec5c
AinRuthPai <tanbing@naver.com>
2022-01-26 18:36:27 +0900

    react-redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { createStore } from "redux";
+import { Provider, useSelector, useDispatch, connect } from "react-redux";
+
+function reducer(currentState, action) {
+  if (currentState === undefined) {
+    return {
+      number: 1,
+    };
+  }
+  const newState = { ...currentState };
+  if (action.type === "PLUS") {
+    newState.number++;
+  }
+  return newState;
+}
+const store = createStore(reducer);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='container'>
+      <h1>Root</h1>
+      <div id='grid'>
+        <Provider store={store}>
+          <Left1 />
+          <Right1 />
+        </Provider>
+      </div>
     </div>
   );
 }
 
+function Left1(props) {
+  return (
+    <div>
+      <h1>Left1</h1>
+      <Left2 />
+    </div>
+  );
+}
+
+function Left2(props) {
+  console.log("2");
+  return (
+    <div>
+      <h1>Left2 </h1>
+      <Left3 />
+    </div>
+  );
+}
+
+function Left3(props) {
+  console.log("3");
+  // function f(state) {
+  //   return state.number;
+  // }
+  const number = useSelector((state) => state.number);
+  return (
+    <div>
+      <h1>Left3 : {number}</h1>
+    </div>
+  );
+}
+
+function Right1(props) {
+  return (
+    <div>
+      <h1>Right1</h1>
+      <Right2 />
+    </div>
+  );
+}
+
+function Right2(props) {
+  return (
+    <div>
+      <h1>Right2</h1>
+      <Right3 />
+    </div>
+  );
+}
+
+function Right3(props) {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1>Right3</h1>
+      <input
+        type='button'
+        value='+'
+        onClick={() => {
+          dispatch({ type: "PLUS" });
+        }}></input>
+    </div>
+  );
+}
 export default App;
